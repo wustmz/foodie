920ce32c994c414fccf0579552449d9676319946
wustmz <865011721@qq.com>
2020-10-18 21:58:16 +0800

    first commit

---
+++ foodie-api/src/main/java/org/foodie/Application.java
@@ -0,0 +1,17 @@
+package org.foodie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author steve.mei
+ * @Version Application,  2020/10/18 9:47 下午
+ **/
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
