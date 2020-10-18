1b748402a16549a3b52ecc8f111528208a92ff77
wustmz <865011721@qq.com>
2020-10-18 22:45:21 +0800

    整合通用mapper

---
+++ foodie-mapper/src/main/java/org/foodie/my/mapper/MyMapper.java
@@ -0,0 +1,12 @@
+package org.foodie.my.mapper;
+
+import tk.mybatis.mapper.common.Mapper;
+import tk.mybatis.mapper.common.MySqlMapper;
+
+/**
+ * 继承自己的MyMapper
+ *
+ * @param <T>
+ */
+public interface MyMapper<T> extends Mapper<T>, MySqlMapper<T> {
+}
