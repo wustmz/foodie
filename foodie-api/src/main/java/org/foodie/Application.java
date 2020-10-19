ff56bab5637a58b15565e99a6dcf3583e048d289
wustmz <865011721@qq.com>
2020-10-19 22:48:29 +0800

    添加包扫描

--- foodie-api/src/main/java/org/foodie/Application.java
+++ foodie-api/src/main/java/org/foodie/Application.java
@@ -1,7 +1,9 @@
 package org.foodie;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @Author steve.mei
@@ -9,6 +11,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  **/
 
 @SpringBootApplication
+@MapperScan(basePackages = "org.foodie.mapper")
+//扫描所有包以及相关组件包
+@ComponentScan(basePackages = {"org.foodie", "org.n3r.idworker"})
 public class Application {
 
     public static void main(String[] args) {
