947d137d910ecec4fc1bad59c10933d6b57fca8c
wustmz <865011721@qq.com>
2020-10-20 23:17:36 +0800

    添加说明

--- foodie-pojo/src/main/java/org/foodie/pojo/bo/UserBo.java
+++ foodie-pojo/src/main/java/org/foodie/pojo/bo/UserBo.java
@@ -1,5 +1,7 @@
 package org.foodie.pojo.bo;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
@@ -7,11 +9,15 @@ import lombok.Data;
  * @Version UserBo,  2020/10/19 9:20 下午
  **/
 @Data
+@ApiModel(value = "用户对象BO", description = "从客户端，由用户传入的数据封装在此entity中")
 public class UserBo {
 
+    @ApiModelProperty(value = "用户名", name = "username", example = "mike", required = true)
     private String username;
 
+    @ApiModelProperty(value = "密码", name = "password", example = "123456", required = true)
     private String password;
 
+    @ApiModelProperty(value = "确认密码", name = "confirmPassword", example = "123456")
     private String confirmPassword;
 }
