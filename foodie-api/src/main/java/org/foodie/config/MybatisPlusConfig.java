00982e242133cf7c32e8c1dd1c5d269dc8254e06
wustmz <865011721@qq.com>
2020-10-20 23:03:46 +0800

    add MybatisPlusConfig

---
+++ foodie-api/src/main/java/org/foodie/config/MybatisPlusConfig.java
@@ -0,0 +1,21 @@
+package org.foodie.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author steve.mei
+ * @Version MybatisPlusConfig,  2020/10/19 11:08 下午
+ **/
+@Configuration
+public class MybatisPlusConfig {
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+}
