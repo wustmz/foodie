bc7c70cb9c7cf25fc8c1c4d052282f5ec19358c0
wustmz <865011721@qq.com>
2020-10-21 19:23:36 +0800

    添加分类服务接口

--- foodie-service/src/main/java/org/foodie/service/ICategoryService.java
+++ foodie-service/src/main/java/org/foodie/service/ICategoryService.java
@@ -2,6 +2,10 @@ package org.foodie.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import org.foodie.pojo.Category;
+import org.foodie.pojo.vo.CategoryVO;
+import org.foodie.pojo.vo.NewItemsVO;
+
+import java.util.List;
 
 /**
  * <p>
@@ -12,5 +16,28 @@ import org.foodie.pojo.Category;
  * @since 2020-10-20
  */
 public interface ICategoryService extends IService<Category> {
+
+    /**
+     * 查询所有一级分类
+     *
+     * @return
+     */
+    List<Category> queryAllRootLevelCat();
+
+    /**
+     * 根据一级分类id查询子分类信息
+     *
+     * @param rootCatId
+     * @return
+     */
+    List<CategoryVO> getSubCatList(Integer rootCatId);
+
+    /**
+     * 查询首页每个一级分类下的6条最新商品数据
+     *
+     * @param rootCatId
+     * @return
+     */
+    List<NewItemsVO> getSixNewItemsLazy(Integer rootCatId);
 
 }
--- foodie-service/src/main/java/org/foodie/service/impl/CategoryServiceImpl.java
+++ foodie-service/src/main/java/org/foodie/service/impl/CategoryServiceImpl.java
@@ -1,10 +1,21 @@
 package org.foodie.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.foodie.mapper.CategoryMapper;
+import org.foodie.mapper.CategoryMapperCustom;
 import org.foodie.pojo.Category;
+import org.foodie.pojo.vo.CategoryVO;
+import org.foodie.pojo.vo.NewItemsVO;
 import org.foodie.service.ICategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -17,4 +28,32 @@ import org.springframework.stereotype.Service;
 @Service
 public class CategoryServiceImpl extends ServiceImpl<CategoryMapper, Category> implements ICategoryService {
 
+    @Autowired
+    private CategoryMapper categoryMapper;
+
+    @Autowired
+    private CategoryMapperCustom categoryMapperCustom;
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public List<Category> queryAllRootLevelCat() {
+        QueryWrapper<Category> wrapper = new QueryWrapper<>();
+        wrapper.lambda().eq(Category::getType, 1);
+        return categoryMapper.selectList(wrapper);
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public List<CategoryVO> getSubCatList(Integer rootCatId) {
+        return categoryMapperCustom.getSubCatList(rootCatId);
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public List<NewItemsVO> getSixNewItemsLazy(Integer rootCatId) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("rootCatId", rootCatId);
+
+        return categoryMapperCustom.getSixNewItemsLazy(map);
+    }
 }
