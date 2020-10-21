625917d278c1c44fbb0796eaa774d1dae9d8854e
wustmz <865011721@qq.com>
2020-10-21 19:23:56 +0800

    添加轮播图服务接口

--- foodie-service/src/main/java/org/foodie/service/ICarouselService.java
+++ foodie-service/src/main/java/org/foodie/service/ICarouselService.java
@@ -3,6 +3,8 @@ package org.foodie.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import org.foodie.pojo.Carousel;
 
+import java.util.List;
+
 /**
  * <p>
  * 轮播图  服务类
@@ -13,4 +15,10 @@ import org.foodie.pojo.Carousel;
  */
 public interface ICarouselService extends IService<Carousel> {
 
+    /**
+     * 查询所有轮播图列表
+     * @param isShow
+     * @return
+     */
+    List<Carousel> queryAll(Integer isShow);
 }
--- foodie-service/src/main/java/org/foodie/service/impl/CarouselServiceImpl.java
+++ foodie-service/src/main/java/org/foodie/service/impl/CarouselServiceImpl.java
@@ -1,10 +1,16 @@
 package org.foodie.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.foodie.mapper.CarouselMapper;
 import org.foodie.pojo.Carousel;
 import org.foodie.service.ICarouselService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +23,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class CarouselServiceImpl extends ServiceImpl<CarouselMapper, Carousel> implements ICarouselService {
 
+    @Autowired
+    private CarouselMapper carouselMapper;
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public List<Carousel> queryAll(Integer isShow) {
+        QueryWrapper<Carousel> wrapper = new QueryWrapper<>();
+        wrapper.lambda().eq(Carousel::getIsShow, isShow);
+        return carouselMapper.selectList(wrapper);
+    }
 }
