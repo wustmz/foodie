6195ff7285919abee2658883fe626c293cfaefa8
wustmz <865011721@qq.com>
2020-10-22 11:57:54 +0800

    添加商品服务实现类

--- foodie-service/src/main/java/org/foodie/service/IItemsService.java
+++ foodie-service/src/main/java/org/foodie/service/IItemsService.java
@@ -2,6 +2,14 @@ package org.foodie.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import org.foodie.pojo.Items;
+import org.foodie.pojo.ItemsImg;
+import org.foodie.pojo.ItemsParam;
+import org.foodie.pojo.ItemsSpec;
+import org.foodie.pojo.vo.CommentLevelCountsVO;
+import org.foodie.pojo.vo.ShopcartVO;
+import org.foodie.utils.PagedGridResult;
+
+import java.util.List;
 
 /**
  * <p>
@@ -13,4 +21,108 @@ import org.foodie.pojo.Items;
  */
 public interface IItemsService extends IService<Items> {
 
+    /**
+     * 根据商品ID查询详情
+     *
+     * @param itemId
+     * @return
+     */
+    Items queryItemById(String itemId);
+
+    /**
+     * 根据商品id查询商品图片列表
+     *
+     * @param itemId
+     * @return
+     */
+    List<ItemsImg> queryItemImgList(String itemId);
+
+    /**
+     * 根据商品id查询商品规格
+     *
+     * @param itemId
+     * @return
+     */
+    List<ItemsSpec> queryItemSpecList(String itemId);
+
+    /**
+     * 根据商品id查询商品参数
+     *
+     * @param itemId
+     * @return
+     */
+    ItemsParam queryItemParam(String itemId);
+
+    /**
+     * 根据商品id查询商品的评价等级数量
+     *
+     * @param itemId
+     */
+    CommentLevelCountsVO queryCommentCounts(String itemId);
+
+    /**
+     * 根据商品id查询商品的评价（分页）
+     *
+     * @param itemId
+     * @param level
+     * @return
+     */
+    PagedGridResult queryPagedComments(String itemId, Integer level,
+                                       Integer page, Integer pageSize);
+
+    /**
+     * 搜索商品列表
+     *
+     * @param keywords
+     * @param sort
+     * @param page
+     * @param pageSize
+     * @return
+     */
+    PagedGridResult searhItems(String keywords, String sort,
+                               Integer page, Integer pageSize);
+
+    /**
+     * 根据分类id搜索商品列表
+     *
+     * @param catId
+     * @param sort
+     * @param page
+     * @param pageSize
+     * @return
+     */
+    PagedGridResult searhItems(Integer catId, String sort,
+                               Integer page, Integer pageSize);
+
+    /**
+     * 根据规格ids查询最新的购物车中商品数据（用于刷新渲染购物车中的商品数据）
+     *
+     * @param specIds
+     * @return
+     */
+    List<ShopcartVO> queryItemsBySpecIds(String specIds);
+
+    /**
+     * 根据商品规格id获取规格对象的具体信息
+     *
+     * @param specId
+     * @return
+     */
+    ItemsSpec queryItemSpecById(String specId);
+
+    /**
+     * 根据商品id获得商品图片主图url
+     *
+     * @param itemId
+     * @return
+     */
+    String queryItemMainImgById(String itemId);
+
+    /**
+     * 减少库存
+     *
+     * @param specId
+     * @param buyCounts
+     */
+    void decreaseItemSpecStock(String specId, int buyCounts);
 }
--- foodie-service/src/main/java/org/foodie/service/impl/ItemsServiceImpl.java
+++ foodie-service/src/main/java/org/foodie/service/impl/ItemsServiceImpl.java
@@ -1,10 +1,26 @@
 package org.foodie.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import org.foodie.mapper.ItemsMapper;
-import org.foodie.pojo.Items;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.foodie.enums.CommentLevel;
+import org.foodie.enums.YesOrNo;
+import org.foodie.mapper.*;
+import org.foodie.pojo.*;
+import org.foodie.pojo.vo.CommentLevelCountsVO;
+import org.foodie.pojo.vo.ItemCommentVO;
+import org.foodie.pojo.vo.SearchItemsVO;
+import org.foodie.pojo.vo.ShopcartVO;
 import org.foodie.service.IItemsService;
+import org.foodie.utils.DesensitizationUtil;
+import org.foodie.utils.PagedGridResult;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.*;
 
 /**
  * <p>
@@ -17,4 +33,175 @@ import org.springframework.stereotype.Service;
 @Service
 public class ItemsServiceImpl extends ServiceImpl<ItemsMapper, Items> implements IItemsService {
 
+    @Autowired
+    private ItemsMapper itemsMapper;
+    @Autowired
+    private ItemsImgMapper itemsImgMapper;
+    @Autowired
+    private ItemsSpecMapper itemsSpecMapper;
+    @Autowired
+    private ItemsParamMapper itemsParamMapper;
+    @Autowired
+    private ItemsCommentsMapper itemsCommentsMapper;
+    @Autowired
+    private ItemsMapperCustom itemsMapperCustom;
+
+    @Override
+    public Items queryItemById(String itemId) {
+        return itemsMapper.selectById(itemId);
+    }
+
+    @Override
+    public List<ItemsImg> queryItemImgList(String itemId) {
+        QueryWrapper<ItemsImg> wrapper = new QueryWrapper<>();
+        wrapper.lambda().eq(ItemsImg::getItemId, itemId);
+        return itemsImgMapper.selectList(wrapper);
+    }
+
+    @Override
+    public List<ItemsSpec> queryItemSpecList(String itemId) {
+        QueryWrapper<ItemsSpec> wrapper = new QueryWrapper<>();
+        wrapper.lambda().eq(ItemsSpec::getItemId, itemId);
+        return itemsSpecMapper.selectList(wrapper);
+    }
+
+    @Override
+    public ItemsParam queryItemParam(String itemId) {
+        QueryWrapper<ItemsParam> wrapper = new QueryWrapper<>();
+        wrapper.lambda().eq(ItemsParam::getItemId, itemId);
+        return itemsParamMapper.selectOne(wrapper);
+    }
+
+    @Override
+    public CommentLevelCountsVO queryCommentCounts(String itemId) {
+        Integer goodCounts = getCommentCounts(itemId, CommentLevel.GOOD.type);
+        Integer normalCounts = getCommentCounts(itemId, CommentLevel.NORMAL.type);
+        Integer badCounts = getCommentCounts(itemId, CommentLevel.BAD.type);
+        Integer totalCounts = goodCounts + normalCounts + badCounts;
+
+        CommentLevelCountsVO countsVO = new CommentLevelCountsVO();
+        countsVO.setTotalCounts(totalCounts);
+        countsVO.setGoodCounts(goodCounts);
+        countsVO.setNormalCounts(normalCounts);
+        countsVO.setBadCounts(badCounts);
+
+        return countsVO;
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    Integer getCommentCounts(String itemId, Integer level) {
+        QueryWrapper<ItemsComments> wrapper = new QueryWrapper<>();
+        wrapper.lambda().eq(ItemsComments::getItemId, itemId);
+        if (level != null) {
+            wrapper.lambda().eq(ItemsComments::getCommentLevel, level);
+        }
+        return itemsCommentsMapper.selectCount(wrapper);
+    }
+
+    @Override
+    public PagedGridResult queryPagedComments(String itemId, Integer level, Integer page, Integer pageSize) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("itemId", itemId);
+        map.put("level", level);
+
+        // mybatis-pagehelper
+
+        /**
+         * page: 第几页
+         * pageSize: 每页显示条数
+         */
+        PageHelper.startPage(page, pageSize);
+
+        List<ItemCommentVO> list = itemsMapperCustom.queryItemComments(map);
+        for (ItemCommentVO vo : list) {
+            vo.setNickname(DesensitizationUtil.commonDisplay(vo.getNickname()));
+        }
+
+        return setterPagedGrid(list, page);
+    }
+
+    private PagedGridResult setterPagedGrid(List<?> list, Integer page) {
+        PageInfo<?> pageList = new PageInfo<>(list);
+        PagedGridResult grid = new PagedGridResult();
+        grid.setPage(page);
+        grid.setRows(list);
+        grid.setTotal(pageList.getPages());
+        grid.setRecords(pageList.getTotal());
+        return grid;
+    }
+
+    @Override
+    public PagedGridResult searhItems(String keywords, String sort, Integer page, Integer pageSize) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("keywords", keywords);
+        map.put("sort", sort);
+
+        PageHelper.startPage(page, pageSize);
+        List<SearchItemsVO> list = itemsMapperCustom.searchItems(map);
+
+        return setterPagedGrid(list, page);
+    }
+
+    @Override
+    public PagedGridResult searhItems(Integer catId, String sort, Integer page, Integer pageSize) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("catId", catId);
+        map.put("sort", sort);
+
+        PageHelper.startPage(page, pageSize);
+        List<SearchItemsVO> list = itemsMapperCustom.searchItemsByThirdCat(map);
+
+        return setterPagedGrid(list, page);
+    }
+
+    @Override
+    public List<ShopcartVO> queryItemsBySpecIds(String specIds) {
+        String ids[] = specIds.split(",");
+        List<String> specIdsList = new ArrayList<>();
+        Collections.addAll(specIdsList, ids);
+
+        return itemsMapperCustom.queryItemsBySpecIds(specIdsList);
+    }
+
+    @Override
+    public ItemsSpec queryItemSpecById(String specId) {
+        return itemsSpecMapper.selectById(specId);
+    }
+
+    @Override
+    public String queryItemMainImgById(String itemId) {
+        QueryWrapper<ItemsImg> wrapper = new QueryWrapper<>();
+        wrapper.lambda()
+                .eq(ItemsImg::getItemId, itemId)
+                .eq(ItemsImg::getIsMain, YesOrNo.YES.type);
+
+        ItemsImg result = itemsImgMapper.selectOne(wrapper);
+        return result != null ? result.getUrl() : "";
+    }
+
+    @Override
+    public void decreaseItemSpecStock(String specId, int buyCounts) {
+        // synchronized 不推荐使用，集群下无用，性能低下
+        // 锁数据库: 不推荐，导致数据库性能低下
+        // 分布式锁 zookeeper redis
+
+        // lockUtil.getLock(); -- 加锁
+
+        // 1. 查询库存
+//        int stock = 10;
+
+        // 2. 判断库存，是否能够减少到0以下
+//        if (stock - buyCounts < 0) {
+        // 提示用户库存不够
+//            10 - 3 -3 - 5 = -1
+//        }
+
+        // lockUtil.unLock(); -- 解锁
+
+
+        int result = itemsMapperCustom.decreaseItemSpecStock(specId, buyCounts);
+        if (result != 1) {
+            throw new RuntimeException("订单创建失败，原因：库存不足!");
+        }
+    }
 }
