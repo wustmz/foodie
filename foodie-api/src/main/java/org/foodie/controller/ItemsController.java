41cb765a47ef0cfbb1ae020c53819974fff95ace
wustmz <865011721@qq.com>
2020-10-22 11:27:14 +0800

    添加商品接口

---
+++ foodie-api/src/main/java/org/foodie/controller/ItemsController.java
@@ -0,0 +1,185 @@
+package org.foodie.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.apache.commons.lang3.StringUtils;
+import org.foodie.pojo.Items;
+import org.foodie.pojo.ItemsImg;
+import org.foodie.pojo.ItemsParam;
+import org.foodie.pojo.ItemsSpec;
+import org.foodie.pojo.vo.CommentLevelCountsVO;
+import org.foodie.pojo.vo.ItemInfoVO;
+import org.foodie.pojo.vo.ShopcartVO;
+import org.foodie.service.IItemsService;
+import org.foodie.utils.PagedGridResult;
+import org.foodie.utils.ServerResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @author wustmz
+ */
+@Api(value = "商品接口", tags = {"商品信息展示的相关接口"})
+@RestController
+@RequestMapping("items")
+public class ItemsController extends BaseController {
+
+    @Autowired
+    private IItemsService itemService;
+
+    @ApiOperation(value = "查询商品详情", notes = "查询商品详情", httpMethod = "GET")
+    @GetMapping("/info/{itemId}")
+    public ServerResponse info(
+            @ApiParam(name = "itemId", value = "商品id", required = true)
+            @PathVariable String itemId) {
+
+        if (StringUtils.isBlank(itemId)) {
+            return ServerResponse.errorMsg(null);
+        }
+
+        Items item = itemService.queryItemById(itemId);
+        List<ItemsImg> itemImgList = itemService.queryItemImgList(itemId);
+        List<ItemsSpec> itemsSpecList = itemService.queryItemSpecList(itemId);
+        ItemsParam itemsParam = itemService.queryItemParam(itemId);
+
+        ItemInfoVO itemInfoVO = new ItemInfoVO();
+        itemInfoVO.setItem(item);
+        itemInfoVO.setItemImgList(itemImgList);
+        itemInfoVO.setItemSpecList(itemsSpecList);
+        itemInfoVO.setItemParams(itemsParam);
+
+        return ServerResponse.ok(itemInfoVO);
+    }
+
+    @ApiOperation(value = "查询商品评价等级", notes = "查询商品评价等级", httpMethod = "GET")
+    @GetMapping("/commentLevel")
+    public ServerResponse commentLevel(
+            @ApiParam(name = "itemId", value = "商品id", required = true)
+            @RequestParam String itemId) {
+
+        if (StringUtils.isBlank(itemId)) {
+            return ServerResponse.errorMsg(null);
+        }
+
+        CommentLevelCountsVO countsVO = itemService.queryCommentCounts(itemId);
+
+        return ServerResponse.ok(countsVO);
+    }
+
+    @ApiOperation(value = "查询商品评论", notes = "查询商品评论", httpMethod = "GET")
+    @GetMapping("/comments")
+    public ServerResponse comments(
+            @ApiParam(name = "itemId", value = "商品id", required = true)
+            @RequestParam String itemId,
+            @ApiParam(name = "level", value = "评价等级", required = false)
+            @RequestParam Integer level,
+            @ApiParam(name = "page", value = "查询下一页的第几页", required = false)
+            @RequestParam Integer page,
+            @ApiParam(name = "pageSize", value = "分页的每一页显示的条数", required = false)
+            @RequestParam Integer pageSize) {
+
+        if (StringUtils.isBlank(itemId)) {
+            return ServerResponse.errorMsg(null);
+        }
+
+        if (page == null) {
+            page = 1;
+        }
+
+        if (pageSize == null) {
+            pageSize = COMMON_PAGE_SIZE;
+        }
+
+        PagedGridResult grid = itemService.queryPagedComments(itemId,
+                level,
+                page,
+                pageSize);
+
+        return ServerResponse.ok(grid);
+    }
+
+    @ApiOperation(value = "搜索商品列表", notes = "搜索商品列表", httpMethod = "GET")
+    @GetMapping("/search")
+    public ServerResponse search(
+            @ApiParam(name = "keywords", value = "关键字", required = true)
+            @RequestParam String keywords,
+            @ApiParam(name = "sort", value = "排序")
+            @RequestParam String sort,
+            @ApiParam(name = "page", value = "查询下一页的第几页")
+            @RequestParam Integer page,
+            @ApiParam(name = "pageSize", value = "分页的每一页显示的条数")
+            @RequestParam Integer pageSize) {
+
+        if (StringUtils.isBlank(keywords)) {
+            return ServerResponse.errorMsg(null);
+        }
+
+        if (page == null) {
+            page = 1;
+        }
+
+        if (pageSize == null) {
+            pageSize = PAGE_SIZE;
+        }
+
+        PagedGridResult grid = itemService.searhItems(keywords,
+                sort,
+                page,
+                pageSize);
+
+        return ServerResponse.ok(grid);
+    }
+
+    @ApiOperation(value = "通过分类id搜索商品列表", notes = "通过分类id搜索商品列表", httpMethod = "GET")
+    @GetMapping("/catItems")
+    public ServerResponse catItems(
+            @ApiParam(name = "catId", value = "三级分类id", required = true)
+            @RequestParam Integer catId,
+            @ApiParam(name = "sort", value = "排序")
+            @RequestParam String sort,
+            @ApiParam(name = "page", value = "查询下一页的第几页")
+            @RequestParam Integer page,
+            @ApiParam(name = "pageSize", value = "分页的每一页显示的条数")
+            @RequestParam Integer pageSize) {
+
+        if (catId == null) {
+            return ServerResponse.errorMsg(null);
+        }
+
+        if (page == null) {
+            page = 1;
+        }
+
+        if (pageSize == null) {
+            pageSize = PAGE_SIZE;
+        }
+
+        PagedGridResult grid = itemService.searhItems(catId, sort, page, pageSize);
+
+        return ServerResponse.ok(grid);
+    }
+
+    /**
+     * 用于用户长时间未登录网站，刷新购物车中的数据（主要是商品价格），类似京东淘宝
+     *
+     * @param itemSpecIds
+     * @return
+     */
+    @ApiOperation(value = "根据商品规格ids查找最新的商品数据", notes = "根据商品规格ids查找最新的商品数据", httpMethod = "GET")
+    @GetMapping("/refresh")
+    public ServerResponse refresh(
+            @ApiParam(name = "itemSpecIds", value = "拼接的规格ids", required = true, example = "1001,1003,1005")
+            @RequestParam String itemSpecIds) {
+
+        if (StringUtils.isBlank(itemSpecIds)) {
+            return ServerResponse.ok();
+        }
+
+        List<ShopcartVO> list = itemService.queryItemsBySpecIds(itemSpecIds);
+
+        return ServerResponse.ok(list);
+    }
+}
