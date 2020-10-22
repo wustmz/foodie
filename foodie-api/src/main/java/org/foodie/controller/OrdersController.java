92dfdced5bbd55d75978ca234eea4c76aece2ce0
wustmz <865011721@qq.com>
2020-10-22 14:02:04 +0800

    添加订单相关接口

---
+++ foodie-api/src/main/java/org/foodie/controller/OrdersController.java
@@ -0,0 +1,105 @@
+package org.foodie.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.foodie.enums.OrderStatusEnum;
+import org.foodie.enums.PayMethod;
+import org.foodie.pojo.OrderStatus;
+import org.foodie.pojo.bo.SubmitOrderBO;
+import org.foodie.pojo.vo.MerchantOrdersVO;
+import org.foodie.pojo.vo.OrderVO;
+import org.foodie.service.IOrdersService;
+import org.foodie.utils.ServerResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Api(value = "订单相关", tags = {"订单相关的api接口"})
+@RequestMapping("orders")
+@RestController
+public class OrdersController extends BaseController {
+
+    final static Logger logger = LoggerFactory.getLogger(org.foodie.controller.OrdersController.class);
+
+    @Autowired
+    private IOrdersService orderService;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @ApiOperation(value = "用户下单", notes = "用户下单", httpMethod = "POST")
+    @PostMapping("/create")
+    public ServerResponse create(
+            @RequestBody SubmitOrderBO submitOrderBO,
+            HttpServletRequest request,
+            HttpServletResponse response) {
+
+        if (submitOrderBO.getPayMethod() != PayMethod.WEIXIN.type
+                && submitOrderBO.getPayMethod() != PayMethod.ALIPAY.type) {
+            return ServerResponse.errorMsg("支付方式不支持！");
+        }
+
+        // 1. 创建订单
+        OrderVO orderVO = orderService.createOrder(submitOrderBO);
+        String orderId = orderVO.getOrderId();
+
+        // 2. 创建订单以后，移除购物车中已结算（已提交）的商品
+        /**
+         * 1001
+         * 2002 -> 用户购买
+         * 3003 -> 用户购买
+         * 4004
+         */
+        // TODO 整合redis之后，完善购物车中的已结算商品清除，并且同步到前端的cookie
+//        CookieUtils.setCookie(request, response, FOODIE_SHOPCART, "", true);
+
+        // 3. 向支付中心发送当前订单，用于保存支付中心的订单数据
+        MerchantOrdersVO merchantOrdersVO = orderVO.getMerchantOrdersVO();
+        merchantOrdersVO.setReturnUrl(payReturnUrl);
+
+        // 为了方便测试购买，所以所有的支付金额都统一改为1分钱
+        merchantOrdersVO.setAmount(1);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.add("imoocUserId", "imooc");
+        headers.add("password", "imooc");
+
+        HttpEntity<MerchantOrdersVO> entity =
+                new HttpEntity<>(merchantOrdersVO, headers);
+
+        ResponseEntity<ServerResponse> responseEntity =
+                restTemplate.postForEntity(paymentUrl,
+                        entity,
+                        ServerResponse.class);
+        ServerResponse paymentResult = responseEntity.getBody();
+        if (paymentResult.getStatus() != 200) {
+            logger.error("发送错误：{}", paymentResult.getMsg());
+            return ServerResponse.errorMsg("支付中心订单创建失败，请联系管理员！");
+        }
+
+        return ServerResponse.ok(orderId);
+    }
+
+    @PostMapping("notifyMerchantOrderPaid")
+    public Integer notifyMerchantOrderPaid(String merchantOrderId) {
+        orderService.updateOrderStatus(merchantOrderId, OrderStatusEnum.WAIT_DELIVER.type);
+        return HttpStatus.OK.value();
+    }
+
+    @PostMapping("getPaidOrderInfo")
+    public ServerResponse getPaidOrderInfo(String orderId) {
+
+        OrderStatus orderStatus = orderService.queryOrderStatusInfo(orderId);
+        return ServerResponse.ok(orderStatus);
+    }
+}
