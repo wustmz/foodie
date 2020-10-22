8f2bd1d2b8e48ca26c6f55322dc30e8f6a0872a9
wustmz <865011721@qq.com>
2020-10-22 14:01:35 +0800

    添加订单服务

--- foodie-service/src/main/java/org/foodie/service/IOrdersService.java
+++ foodie-service/src/main/java/org/foodie/service/IOrdersService.java
@@ -1,7 +1,10 @@
 package org.foodie.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import org.foodie.pojo.OrderStatus;
 import org.foodie.pojo.Orders;
+import org.foodie.pojo.bo.SubmitOrderBO;
+import org.foodie.pojo.vo.OrderVO;
 
 /**
  * <p>
@@ -13,4 +16,31 @@ import org.foodie.pojo.Orders;
  */
 public interface IOrdersService extends IService<Orders> {
 
+    /**
+     * 用于创建订单相关信息
+     *
+     * @param submitOrderBO
+     */
+    OrderVO createOrder(SubmitOrderBO submitOrderBO);
+
+    /**
+     * 修改订单状态
+     *
+     * @param orderId
+     * @param orderStatus
+     */
+    void updateOrderStatus(String orderId, Integer orderStatus);
+
+    /**
+     * 查询订单状态
+     *
+     * @param orderId
+     * @return
+     */
+    OrderStatus queryOrderStatusInfo(String orderId);
+
+    /**
+     * 关闭超时未支付订单
+     */
+    void closeOrder();
 }
--- foodie-service/src/main/java/org/foodie/service/impl/OrdersServiceImpl.java
+++ foodie-service/src/main/java/org/foodie/service/impl/OrdersServiceImpl.java
@@ -1,10 +1,30 @@
 package org.foodie.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.foodie.enums.OrderStatusEnum;
+import org.foodie.enums.YesOrNo;
+import org.foodie.mapper.OrderItemsMapper;
+import org.foodie.mapper.OrderStatusMapper;
 import org.foodie.mapper.OrdersMapper;
-import org.foodie.pojo.Orders;
+import org.foodie.pojo.*;
+import org.foodie.pojo.bo.SubmitOrderBO;
+import org.foodie.pojo.vo.MerchantOrdersVO;
+import org.foodie.pojo.vo.OrderVO;
+import org.foodie.service.IItemsService;
 import org.foodie.service.IOrdersService;
+import org.foodie.service.IUserAddressService;
+import org.foodie.utils.DateUtil;
+import org.n3r.idworker.Sid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +37,174 @@ import org.springframework.stereotype.Service;
 @Service
 public class OrdersServiceImpl extends ServiceImpl<OrdersMapper, Orders> implements IOrdersService {
 
+    @Autowired
+    private OrdersMapper ordersMapper;
+
+    @Autowired
+    private OrderItemsMapper orderItemsMapper;
+
+    @Autowired
+    private OrderStatusMapper orderStatusMapper;
+
+    @Autowired
+    private IUserAddressService addressService;
+
+    @Autowired
+    private IItemsService itemService;
+
+    @Autowired
+    private Sid sid;
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    @Override
+    public OrderVO createOrder(SubmitOrderBO submitOrderBO) {
+        String userId = submitOrderBO.getUserId();
+        String addressId = submitOrderBO.getAddressId();
+        String itemSpecIds = submitOrderBO.getItemSpecIds();
+        Integer payMethod = submitOrderBO.getPayMethod();
+        String leftMsg = submitOrderBO.getLeftMsg();
+        // 包邮费用设置为0
+        Integer postAmount = 0;
+
+        String orderId = sid.nextShort();
+
+        UserAddress address = addressService.queryUserAddres(userId, addressId);
+
+        // 1. 新订单数据保存
+        Orders newOrder = new Orders();
+        newOrder.setId(orderId);
+        newOrder.setUserId(userId);
+
+        newOrder.setReceiverName(address.getReceiver());
+        newOrder.setReceiverMobile(address.getMobile());
+        newOrder.setReceiverAddress(address.getProvince() + " "
+                + address.getCity() + " "
+                + address.getDistrict() + " "
+                + address.getDetail());
+
+        newOrder.setPostAmount(postAmount);
+
+        newOrder.setPayMethod(payMethod);
+        newOrder.setLeftMsg(leftMsg);
+
+        newOrder.setIsComment(YesOrNo.NO.type);
+        newOrder.setIsDelete(YesOrNo.NO.type);
+        newOrder.setCreatedTime(LocalDateTime.now());
+        newOrder.setUpdatedTime(LocalDateTime.now());
+
+
+        // 2. 循环根据itemSpecIds保存订单商品信息表
+        String itemSpecIdArr[] = itemSpecIds.split(",");
+        Integer totalAmount = 0;    // 商品原价累计
+        Integer realPayAmount = 0;  // 优惠后的实际支付价格累计
+        for (String itemSpecId : itemSpecIdArr) {
+
+            // TODO 整合redis后，商品购买的数量重新从redis的购物车中获取
+            int buyCounts = 1;
+
+            // 2.1 根据规格id，查询规格的具体信息，主要获取价格
+            ItemsSpec itemSpec = itemService.queryItemSpecById(itemSpecId);
+            totalAmount += itemSpec.getPriceNormal() * buyCounts;
+            realPayAmount += itemSpec.getPriceDiscount() * buyCounts;
+
+            // 2.2 根据商品id，获得商品信息以及商品图片
+            String itemId = itemSpec.getItemId();
+            Items item = itemService.queryItemById(itemId);
+            String imgUrl = itemService.queryItemMainImgById(itemId);
+
+            // 2.3 循环保存子订单数据到数据库
+            String subOrderId = sid.nextShort();
+            OrderItems subOrderItem = new OrderItems();
+            subOrderItem.setId(subOrderId);
+            subOrderItem.setOrderId(orderId);
+            subOrderItem.setItemId(itemId);
+            subOrderItem.setItemName(item.getItemName());
+            subOrderItem.setItemImg(imgUrl);
+            subOrderItem.setBuyCounts(buyCounts);
+            subOrderItem.setItemSpecId(itemSpecId);
+            subOrderItem.setItemSpecName(itemSpec.getName());
+            subOrderItem.setPrice(itemSpec.getPriceDiscount());
+            orderItemsMapper.insert(subOrderItem);
+
+            // 2.4 在用户提交订单以后，规格表中需要扣除库存
+            itemService.decreaseItemSpecStock(itemSpecId, buyCounts);
+        }
+
+        newOrder.setTotalAmount(totalAmount);
+        newOrder.setRealPayAmount(realPayAmount);
+        ordersMapper.insert(newOrder);
+
+        // 3. 保存订单状态表
+        OrderStatus waitPayOrderStatus = new OrderStatus();
+        waitPayOrderStatus.setOrderId(orderId);
+        waitPayOrderStatus.setOrderStatus(OrderStatusEnum.WAIT_PAY.type);
+        waitPayOrderStatus.setCreatedTime(LocalDateTime.now());
+        orderStatusMapper.insert(waitPayOrderStatus);
+
+        // 4. 构建商户订单，用于传给支付中心
+        MerchantOrdersVO merchantOrdersVO = new MerchantOrdersVO();
+        merchantOrdersVO.setMerchantOrderId(orderId);
+        merchantOrdersVO.setMerchantUserId(userId);
+        merchantOrdersVO.setAmount(realPayAmount + postAmount);
+        merchantOrdersVO.setPayMethod(payMethod);
+
+        // 5. 构建自定义订单vo
+        OrderVO orderVO = new OrderVO();
+        orderVO.setOrderId(orderId);
+        orderVO.setMerchantOrdersVO(merchantOrdersVO);
+
+        return orderVO;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    @Override
+    public void updateOrderStatus(String orderId, Integer orderStatus) {
+        OrderStatus paidStatus = new OrderStatus();
+        paidStatus.setOrderId(orderId);
+        paidStatus.setOrderStatus(orderStatus);
+        paidStatus.setPayTime(LocalDateTime.now());
+
+        UpdateWrapper<OrderStatus> wrapper = new UpdateWrapper<>();
+        wrapper.lambda().eq(OrderStatus::getOrderId, orderId);
+
+        orderStatusMapper.update(paidStatus, wrapper);
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public OrderStatus queryOrderStatusInfo(String orderId) {
+        return orderStatusMapper.selectById(orderId);
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    @Override
+    public void closeOrder() {
+        // 查询所有未付款订单，判断时间是否超时（1天），超时则关闭交易
+        QueryWrapper<OrderStatus> wrapper = new QueryWrapper<>();
+        wrapper.lambda().eq(OrderStatus::getOrderStatus, OrderStatusEnum.WAIT_PAY.type);
+
+        List<OrderStatus> list = orderStatusMapper.selectList(wrapper);
+        for (OrderStatus os : list) {
+            // 获得订单创建时间
+            LocalDateTime createdTime = os.getCreatedTime();
+            // 和当前时间进行对比
+            int days = DateUtil.daysBetween(createdTime, LocalDateTime.now());
+            if (days >= 1) {
+                // 超过1天，关闭订单
+                doCloseOrder(os.getOrderId());
+            }
+        }
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    void doCloseOrder(String orderId) {
+        OrderStatus close = new OrderStatus();
+        close.setOrderId(orderId);
+        close.setOrderStatus(OrderStatusEnum.CLOSE.type);
+        close.setCloseTime(LocalDateTime.now());
+
+        UpdateWrapper<OrderStatus> wrapper = new UpdateWrapper<>();
+        wrapper.lambda().eq(OrderStatus::getOrderId, orderId);
+        orderStatusMapper.update(close, wrapper);
+    }
 }
