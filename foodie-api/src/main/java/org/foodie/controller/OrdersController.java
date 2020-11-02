01762d72d02dd63dbab54b94b38e4d88681ab0bd
wustmz <865011721@qq.com>
2020-11-02 23:36:00 +0800

    optimize import

--- foodie-api/src/main/java/org/foodie/controller/OrdersController.java
+++ foodie-api/src/main/java/org/foodie/controller/OrdersController.java
@@ -19,7 +19,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
-import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
