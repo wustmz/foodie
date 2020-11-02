3f26fdc4324281b89f21a8a039c33f0f90761026
wustmz <865011721@qq.com>
2020-11-02 23:07:50 +0800

    基于redis优化购物车

--- foodie-api/src/main/java/org/foodie/controller/ShopcatController.java
+++ foodie-api/src/main/java/org/foodie/controller/ShopcatController.java
@@ -86,7 +86,22 @@ public class ShopcatController extends BaseController {
             return ServerResponse.errorMsg("参数不能为空");
         }
 
-        // TODO 用户在页面删除购物车中的商品数据，如果此时用户已经登录，则需要同步删除后端购物车中的商品
+        //用户在页面删除购物车中的商品数据，如果此时用户已经登录，则需要同步删除redis购物车中的商品
+        String shopCartJson = redisOperator.get(FOODIE_SHOPCART + ":" + userId);
+        if (StringUtils.isNotBlank(shopCartJson)) {
+            //redis中已经有购物车了
+            List<ShopcartBO> shopCartList = JsonUtils.jsonToList(shopCartJson, ShopcartBO.class);
+            //判断购物车中是否存在已有商品，如果有的话则删除
+            for (ShopcartBO sc : shopCartList) {
+                String tmpSpecId = sc.getSpecId();
+                if (tmpSpecId.equals(itemSpecId)) {
+                    shopCartList.remove(sc);
+                    break;
+                }
+            }
+            //覆盖现有redis中的购物车
+            redisOperator.set(FOODIE_SHOPCART + ":" + userId, JsonUtils.objectToJson(shopCartList));
+        }
 
         return ServerResponse.ok();
     }
