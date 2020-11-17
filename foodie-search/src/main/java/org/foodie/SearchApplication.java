380f4f8d1609f76fe680b2a749fbbd7b8686a3b9
wustmz <865011721@qq.com>
2020-11-17 21:24:58 +0800

    添加启动类

---
+++ foodie-search/src/main/java/org/foodie/SearchApplication.java
@@ -0,0 +1,17 @@
+package org.foodie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author steve.mei
+ * @Version SSOApplication,  2020/10/18 9:47 下午
+ **/
+
+@SpringBootApplication
+public class SearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchApplication.class, args);
+    }
+}
